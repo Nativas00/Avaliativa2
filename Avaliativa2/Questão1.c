9ed0f64bed2b1b3b3c2079495ccce21ed88fd55f
Nativas00 <134939344+Nativas00@users.noreply.github.com>
2023-11-06 12:02:16 -0300

    Add files via upload

---
+++ Avaliativa2/Questão1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+  
+  int X[10];
+  int Y[10];
+  int Tamanho = 10;
+
+  for (int i = 0; i < 10; i++){
+    scanf("%d", &X[i]);
+    Y[i] = X[i];
+  }
+  
+  for (int i = 0; i < 10; i++){
+    printf("%d", X[i]);
+    if (i < 10) {
+      printf(" ");
+    }
+  }
+  
+  printf("\n");
+  
+  while (Tamanho > 1){
+    for (int i = 0; i < Tamanho - 1; i++){
+      Y[i] = X[i] + X[i + 1];
+    }
+    Tamanho--;
+    for (int i = 0; i < Tamanho; i++){
+      printf("%d", X[i]);
+      if (i < Tamanho - 1){
+        printf(" ");
+      }
+    }
+    
+    printf("\n");
+    for (int i = 0; i < Tamanho; i++){
+      X[i] = Y[i];
+    }
+  }
+  
+return 0;
+}
---
+++ Avaliativa2/Questão3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int X[4][4];
+    int Y[4][4]; 
+    int R[4][4];
+    char Calculo[16];
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            scanf("%d", &X[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            scanf("%d", &Y[i][j]);
+        }
+    }
+
+    scanf("%s", Calculo);
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(strcmp(Calculo, "soma") == 0){
+                R[i][j] = X[i][j] + Y[i][j];
+            } 
+            else if(strcmp(Calculo, "sub") == 0){
+                R[i][j] = X[i][j] - Y[i][j];
+            } 
+            else if(strcmp(Calculo, "mult") == 0){
+              R[i][j] = 0;
+              for (int l = 0; l < 4; l++){
+                  R[i][j] += X[i][l] * Y[l][j];
+              }
+            }
+            else{
+              printf("Não foi possivel gerar a matriz!!\n");
+            }
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%3d", R[i][j]);
+          
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
