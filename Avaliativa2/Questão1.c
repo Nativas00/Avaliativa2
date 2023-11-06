8f704c1e5b8d158d9514e6d6cab1bc0d1db8c930
Nativas00 <134939344+Nativas00@users.noreply.github.com>
2023-11-06 19:43:17 -0300

    Update Questão1.c

--- Avaliativa2/Questão1.c
+++ Avaliativa2/Questão1.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 
-int main(){
-  
+int main() {
+    
   int X[10];
   int Y[10];
   int Tamanho = 10;
 
-  for (int i = 0; i < 10; i++){
+  for(int i = 0; i < 10; i++){
     scanf("%d", &X[i]);
     Y[i] = X[i];
   }
-  
-  for (int i = 0; i < 10; i++){
+
+  for(int i = 0; i < 10; i++){
     printf("%d", X[i]);
-    if (i < 10){
+    if (i < 9){
       printf(" ");
     }
   }
   printf("\n");
-  
-  while (Tamanho > 1){
-    for (int i = 0; i < Tamanho - 1; i++){
+
+  while(Tamanho > 1){
+    for(int i = 0; i < Tamanho - 1; i++){
       Y[i] = X[i] + X[i + 1];
     }
     Tamanho--;
-    for (int i = 0; i < Tamanho; i++){
-      printf("%d", X[i]);
+    for(int i = 0; i < Tamanho; i++){
+      printf("%d", Y[i]);
       if (i < Tamanho - 1){
         printf(" ");
       }
     }
     printf("\n");
-    
+
     for (int i = 0; i < Tamanho; i++){
       X[i] = Y[i];
     }
   }
-  
   return 0;
+  
 }
+
