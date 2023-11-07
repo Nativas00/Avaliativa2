f44302397abf44cf2254bd0021ed993303cb9351
Nativas00 <134939344+Nativas00@users.noreply.github.com>
2023-11-06 23:28:50 -0300

    Update Questão3.c

--- Avaliativa2/Questão3.c
+++ Avaliativa2/Questão3.c
@@ -43,7 +43,7 @@ int main(){
 
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
-            printf("%3d", R[i][j]);
+            printf("%4d", R[i][j]);
           
         }
         printf("\n");
